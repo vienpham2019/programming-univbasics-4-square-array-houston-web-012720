956a7832639747069cf22f5e6cb8ccf5f0e11d76
vien pham <vienpham2019@gmail.com>
2019-12-08 03:57:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   # your code here
   count = 0
   while count < array.length do
-    array[count] ** 2
+    array[count] = array[count] ** 2
     count++
   end
+  return array
 end
